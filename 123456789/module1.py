04d7066cab43396441f6c6bea056705ab944ec22
Winzler-Botawar <kilokillentak@outlook.com>
2021-11-21 13:30:05 +0800

    3.0

--- 123456789/module1.py
+++ 123456789/module1.py
@@ -5,23 +5,35 @@ import numpy as np
 #fig = plt.figure(figsize=(6,6))\
 
 
-class surface(object):
-    def __init__(self, col, row):
-        self.col = col
-        self.row = row
+class surface(object):                          # 所有法向量都應由用戶自行標注
+    def __init__(self, p1, p2, n):
+        self.p1 = p1
+        self.p2 = p2
+        self.x_list = []
+        self.y_list = []
+        vec_n = n
+        a = np.mat((p2[0] - p1[0]), (p2[1] - p1[0]))
+        print(a.shape())
 
-    #def Generate_Surface(self):
-    #    step_x = (self.p2[0] - self.p1[0])/50
-    #    step_y = (self.p2[1] - self.p1[1])/50
-    def create(self):
-        col_, row_ = np.meshgrid(np.arange(self.col),np.arange(self.row))
-        print(col_.shape)
+    def Generate_Surface(self):
+        step_x = (self.p2[0] - self.p1[0])/50
+        step_y = (self.p2[1] - self.p1[1])/50
+        i = self.p1[0]
+        while i < self.p2[0]:
+            i += step_x
+            self.x_list.append(i)
+        print(self.x_list)
+    #def create(self):
+    #    x,y = map(int,input().split())
+    #    a = np.ones((x+1,y+1))
+
+    #    col_, row_ = np.meshgrid(np.arange(self.col),np.arange(self.row))
+    #    print(col_.shape)
 
 
 
 
-class furniture():
-
+class furniture(object):
     pass
 
 class door(furniture):
@@ -32,5 +44,11 @@ def init_energy_function(p1,p2,*door):
 
 
 if __name__ == "__main__":
-    a = surface(10,30)
-    a.create()
+    #plt.figure(figsize=(5, 5)) #设置坐标轴的大小
+    #plt.title("Room Coordinate")  #设置标题
+    #plt.show()  #展示
+    p_1 = [5,3]
+    p_2 = [7,6]
+    x = [2,3]
+    a = surface(p_1,p_2,x)
+    a.Generate_Surface()
